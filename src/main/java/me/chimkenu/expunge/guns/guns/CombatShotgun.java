b27bf06afad5beb5b9837db533b3632702490c30
BuildTools <unconfigured@null.spigotmc.org>
2022-06-06 10:48:43 +0800

    Shotgun balancing

--- src/main/java/me/chimkenu/expunge/guns/guns/CombatShotgun.java
+++ src/main/java/me/chimkenu/expunge/guns/guns/CombatShotgun.java
@@ -6,6 +6,6 @@ import org.bukkit.Sound;
 
 public class CombatShotgun extends Gun {
     public CombatShotgun() {
-        super(6, 9, 10, 3, 60, 10, 100, 1, Particle.SMOKE_NORMAL, Material.GOLDEN_AXE, "&eCombat Shotgun", Sound.ENTITY_GENERIC_EXPLODE, 2);
+        super(6, 9, 10, 3, 60, 10, 100, 4, Particle.SMOKE_NORMAL, Material.GOLDEN_AXE, "&eCombat Shotgun", Sound.ENTITY_GENERIC_EXPLODE, 2);
     }
 }
