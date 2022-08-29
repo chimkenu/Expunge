51267c844d94e10e1a9fe62404f3d0d5495a8bf7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 22:28:15 +0800

    added the witch

--- src/main/java/me/chimkenu/expunge/mobs/special/Witch.java
+++ src/main/java/me/chimkenu/expunge/mobs/special/Witch.java
@@ -3,16 +3,25 @@ package me.chimkenu.expunge.mobs.special;
 import me.chimkenu.expunge.mobs.GameMob;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Mob;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Witch extends GameMob {
     public <T extends Mob> Witch(World world, Location locationToSpawn) {
         super(world, locationToSpawn, Enderman.class, mob -> {
             if (mob.getTarget() != null) {
-
+                mob.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20, 200, false, false, false));
             }
         });
-        getMob().setAbsorptionAmount(1);
+        try {
+            getMob().getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(39);
+            getMob().getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(1000);
+            getMob().setHealth(1000);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
