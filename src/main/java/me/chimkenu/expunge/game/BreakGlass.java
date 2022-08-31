cbb0f3b2172156ae3a4e5f5465353d4bf211eb0c
chimkenu <106415170+chimkenu@users.noreply.github.com>
2022-08-31 18:25:44 +0800

    fixed BreakGlass.java

--- src/main/java/me/chimkenu/expunge/game/BreakGlass.java
+++ src/main/java/me/chimkenu/expunge/game/BreakGlass.java
@@ -1,27 +1,29 @@
 package me.chimkenu.expunge.game;
 
-import me.chimkenu.expunge.Expunge;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class BreakGlass {
-    private Set<Block> glassLocations = new Set<>();
+    private static final HashSet<Block> glassLocations = new HashSet<>();
 
     public static void returnGlass() {
         for (Block block : glassLocations) {
-            block.setBlock(block);
+            block.getLocation().getBlock().setType(block.getType());
+            block.getLocation().getBlock().setBlockData(block.getBlockData());
         }
         glassLocations.clear();
     }
 
-    public static void breakGlass() {
-        if (block.getMaterial() == Material.GLASS_BLOCK or block.getMaterial() == Material.GLASS_PANE) {
-            block.getWorld().spawnParticle(Particle.BLOCK);
-            block.getWorld().playSound(Sound.BLOCK_GLASS_BREAK);
+    public static void breakGlass(Block block) {
+        if (block.getType() == Material.GLASS || block.getType() == Material.GLASS_PANE) {
+            block.getWorld().spawnParticle(Particle.BLOCK_CRACK, block.getLocation().add(0, .5, 0), 15, 0.2, 0.2, 0.2, block.getType().createBlockData());
+            block.getWorld().playSound(block.getLocation(), Sound.BLOCK_GLASS_BREAK, 1, 1);
             glassLocations.add(block);
-            block.setBlock(Material.AIR);
+            block.setType(Material.AIR);
         }
-        else
-            throw new IllegalArgumentException();
     }
 }
