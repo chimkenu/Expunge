1c834b74eec939f51baa0340e1a7096f45d0afd9
chimkenu <106415170+chimkenu@users.noreply.github.com>
2023-11-05 13:23:45 +0800

    Tracking player stats with PlayerStats

---
+++ src/main/java/me/chimkenu/expunge/game/PlayerStats.java
@@ -0,0 +1,70 @@
+package me.chimkenu.expunge.game;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class PlayerStats {
+    private double health;
+    private double absorption;
+    private ItemStack[] hotbar;
+    private boolean isAlive;
+    private int lives;
+
+    public PlayerStats(ItemStack[] hotbar) {
+        health = 20d;
+        absorption = 0d;
+        this.hotbar = hotbar;
+        isAlive = true;
+        lives = 3;
+    }
+
+    public PlayerStats() {
+        this(new ItemStack[5]);
+        Arrays.fill(hotbar, new ItemStack(Material.AIR));
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public double getAbsorption() {
+        return absorption;
+    }
+
+    public void setAbsorption(double absorption) {
+        this.absorption = absorption;
+    }
+
+    public ItemStack[] getHotbar() {
+        return hotbar;
+    }
+
+    public void setHotbar(ItemStack[] hotbar) {
+        this.hotbar = hotbar;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+}
