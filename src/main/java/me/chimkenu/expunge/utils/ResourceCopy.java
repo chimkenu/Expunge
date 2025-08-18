4f8ec9ed888a69684347bb3393079bf782d31df5
chimkenu <106415170+chimkenu@users.noreply.github.com>
2025-08-18 18:16:59 +1000

    Moved block inside try-with resources and added not null annotations

--- src/main/java/me/chimkenu/expunge/utils/ResourceCopy.java
+++ src/main/java/me/chimkenu/expunge/utils/ResourceCopy.java
@@ -1,5 +1,7 @@
 package me.chimkenu.expunge.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,26 +13,29 @@ import java.util.Objects;
 public final class ResourceCopy {
     public static void copyFromJar(Class<?> clazz, String source, final Path target) throws URISyntaxException, IOException {
         URI resource = Objects.requireNonNull(clazz.getResource("")).toURI();
-        FileSystem fileSystem = FileSystems.newFileSystem(
+        try (FileSystem fileSystem = FileSystems.newFileSystem(
                 resource,
                 Collections.<String, String>emptyMap()
-        );
+        )) {
 
-        final Path jarPath = fileSystem.getPath(source);
+            final Path jarPath = fileSystem.getPath(source);
 
-        Files.walkFileTree(jarPath, new SimpleFileVisitor<>() {
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                Path currentTarget = target.resolve(jarPath.relativize(dir).toString());
-                Files.createDirectories(currentTarget);
-                return FileVisitResult.CONTINUE;
-            }
+            Files.walkFileTree(jarPath, new SimpleFileVisitor<>() {
+                @NotNull
+                @Override
+                public FileVisitResult preVisitDirectory(@NotNull Path dir, @NotNull BasicFileAttributes attrs) throws IOException {
+                    Path currentTarget = target.resolve(jarPath.relativize(dir).toString());
+                    Files.createDirectories(currentTarget);
+                    return FileVisitResult.CONTINUE;
+                }
 
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                Files.copy(file, target.resolve(jarPath.relativize(file).toString()), StandardCopyOption.REPLACE_EXISTING);
-                return FileVisitResult.CONTINUE;
-            }
-        });
+                @NotNull
+                @Override
+                public FileVisitResult visitFile(@NotNull Path file, @NotNull BasicFileAttributes attrs) throws IOException {
+                    Files.copy(file, target.resolve(jarPath.relativize(file).toString()), StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
     }
 }
