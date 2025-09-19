8b0ee73b80bfafbe6f928675c6417372cf647eea
chimkenu <106415170+chimkenu@users.noreply.github.com>
2025-09-20 01:35:15 +1000

    included shove time in player stats

--- src/main/java/me/chimkenu/expunge/game/PlayerStats.java
+++ src/main/java/me/chimkenu/expunge/game/PlayerStats.java
@@ -1,29 +1,36 @@
 package me.chimkenu.expunge.game;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 
 public class PlayerStats {
+    private final int MAX_LIVES = 3;
+
+    // game stats
     private double health;
     private double absorption;
     private ItemStack[] hotbar;
     private boolean isAlive;
     private int lives;
     private int livesFromLastSave;
-    private long timeSinceLastShove;
-    private int numberOfRecentShoves;
+
+    // skill stats
+    private int commonInfectedKills;
+    private int specialInfectedKills;
+    private int shots;
+    private int shotsHit;
+    private int headshots;
 
     public PlayerStats(ItemStack[] hotbar) {
         health = 20d;
         absorption = 0d;
         this.hotbar = hotbar;
         isAlive = true;
-        lives = 3;
-        livesFromLastSave = 3;
-        timeSinceLastShove = 0;
-        numberOfRecentShoves = 0;
+        lives = MAX_LIVES;
+        livesFromLastSave = MAX_LIVES;
     }
 
     public PlayerStats() {
@@ -36,21 +43,18 @@ public class PlayerStats {
         setAbsorption(0);
         Arrays.fill(hotbar, new ItemStack(Material.AIR));
         setAlive(true);
-        setLives(3);
+        setLives(MAX_LIVES);
     }
 
     public double getHealth() {
         return health;
     }
-
     public void setHealth(double health) {
         this.health = health;
     }
-
     public double getAbsorption() {
         return absorption;
     }
-
     public void setAbsorption(double absorption) {
         this.absorption = absorption;
     }
@@ -58,7 +62,6 @@ public class PlayerStats {
     public ItemStack[] getHotbar() {
         return hotbar;
     }
-
     public void setHotbar(ItemStack[] hotbar) {
         this.hotbar = hotbar;
     }
@@ -66,40 +69,78 @@ public class PlayerStats {
     public boolean isAlive() {
         return isAlive;
     }
-
     public void setAlive(boolean alive) {
         isAlive = alive;
     }
-
     public int getLives() {
         return lives;
     }
-
     public void setLives(int lives) {
         this.lives = lives;
     }
-
+    public void resetLives() {
+        this.lives = MAX_LIVES;
+    }
     public int getLivesFromLastSave() {
         return livesFromLastSave;
     }
-
     public void setLivesFromLastSave(int livesFromLastSave) {
         this.livesFromLastSave = livesFromLastSave;
     }
 
-    public long getTimeSinceLastShove() {
-        return timeSinceLastShove;
+    public int getCommonKills() {
+        return commonInfectedKills;
+    }
+    public void setCommonKills(int kills) {
+        commonInfectedKills = kills;
+    }
+    public void addCommonKill() {
+        commonInfectedKills++;
+    }
+    public int getSpecialKills() {
+        return specialInfectedKills;
+    }
+    public void setSpecialKills(int kills) {
+        specialInfectedKills = kills;
+    }
+    public void addSpecialKill() {
+        specialInfectedKills++;
     }
 
-    public void setTimeSinceLastShove(long timeSinceLastShove) {
-        this.timeSinceLastShove = timeSinceLastShove;
+    public double getAccuracy() {
+        if (shots == 0) return 0;
+        return (double) shotsHit / shots;
     }
 
-    public int getNumberOfRecentShoves() {
-        return numberOfRecentShoves;
+    public double getHeadshotAccuracy() {
+        if (shotsHit == 0) return 0;
+        return (double) headshots / shotsHit;
     }
 
-    public void setNumberOfRecentShoves(int numberOfRecentShoves) {
-        this.numberOfRecentShoves = numberOfRecentShoves;
+    public void addShot(boolean shotHit, boolean headshot) {
+        shots += 1;
+        shotsHit += shotHit ? 1 : 0;
+        headshots += headshot ? 1 : 0;
+    }
+
+    public void savePlayerStats(Player p) {
+        setHealth(p.getHealth());
+        setAbsorption(p.getAbsorptionAmount());
+        ItemStack[] itemStacks = getHotbar();
+        for (int i = 0; i < 5; i++) {
+            itemStacks[i] = p.getInventory().getItem(i);
+        }
+        setHotbar(itemStacks);
+        setLivesFromLastSave(getLives());
+    }
+
+    public void loadPlayerStats(Player p) {
+        p.setHealth(getHealth());
+        p.setAbsorptionAmount(getAbsorption());
+        p.getInventory().clear();
+        for (int i = 0; i < getHotbar().length; i++) {
+            p.getInventory().setItem(i, getHotbar()[i]);
+        }
+        setLives(getLivesFromLastSave());
     }
 }
