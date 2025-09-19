92fc46b54879a3db4367d3fa55dd25eef40dbb27
chimkenu <106415170+chimkenu@users.noreply.github.com>
2025-09-19 21:50:55 +1000

    feat: new melee system

--- src/main/java/me/chimkenu/expunge/items/Melee.java
+++ src/main/java/me/chimkenu/expunge/items/Melee.java
@@ -4,14 +4,16 @@ import me.chimkenu.expunge.enums.MeleeType;
 import me.chimkenu.expunge.enums.Slot;
 import me.chimkenu.expunge.enums.Tier;
 import me.chimkenu.expunge.utils.ChatUtil;
+import me.chimkenu.expunge.utils.ItemUtil;
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
+
+import java.util.List;
 
 public record Melee(
         String id,
@@ -28,32 +30,57 @@ public record Melee(
         MeleeType.Attack attackType,
         MeleeType.Damage damageType
 ) implements Weapon {
+    public static final double BLOCK_BREAK_SPEED = 3;
+
     public Melee {
         if (slot == null) slot = Slot.SECONDARY;
+        if (damageType == MeleeType.Damage.RELATIVE) damage = 21;
     }
 
     @Override
     public ItemStack toItem() {
         ItemStack melee = new ItemStack(material());
+        melee = ItemUtil.setDestroyable(melee, List.of(Material.GLASS, Material.GLASS_PANE));
         ItemMeta meta = melee.getItemMeta();
 
-        final double DEFAULT_REACH = 3.0; // vanilla default
-        double adjustment = range() - DEFAULT_REACH;
+        final double DEFAULT_REACH = 3.0;
+        double reach = range() - DEFAULT_REACH;
+
+        final double DEFAULT_ATTACK_SPEED = 4.0;
+        double attack_speed = -1 * DEFAULT_ATTACK_SPEED * (swapCooldown() / 30d);
 
         if (meta != null) {
             meta.setDisplayName(ChatUtil.format(name()));
             meta.getPersistentDataContainer().set(namespacedKey(), PersistentDataType.BOOLEAN, true);
 
-            AttributeModifier modifier = new AttributeModifier(
+            meta.addAttributeModifier(Attribute.ENTITY_INTERACTION_RANGE, new AttributeModifier(
                     namespacedKey(),
-                    adjustment,
+                    reach,
                     AttributeModifier.Operation.ADD_NUMBER,
-                    EquipmentSlotGroup.HAND
-            );
+                    EquipmentSlotGroup.MAINHAND
+            ));
+            meta.addAttributeModifier(Attribute.ATTACK_DAMAGE, new AttributeModifier(
+                    namespacedKey(),
+                    damage(),
+                    AttributeModifier.Operation.ADD_NUMBER,
+                    EquipmentSlotGroup.MAINHAND
+            ));
+            meta.addAttributeModifier(Attribute.BLOCK_BREAK_SPEED, new AttributeModifier(
+                    namespacedKey(),
+                    BLOCK_BREAK_SPEED,
+                    AttributeModifier.Operation.ADD_NUMBER,
+                    EquipmentSlotGroup.MAINHAND
+            ));
+            meta.addAttributeModifier(Attribute.ATTACK_SPEED, new AttributeModifier(
+                    namespacedKey(),
+                    attack_speed,
+                    AttributeModifier.Operation.ADD_NUMBER,
+                    EquipmentSlotGroup.MAINHAND
+            ));
 
-            meta.addAttributeModifier(Attribute.ENTITY_INTERACTION_RANGE, modifier);
             melee.setItemMeta(meta);
         }
+
         return melee;
     }
 }
--- src/main/java/me/chimkenu/expunge/listeners/game/SwingListener.java
+++ src/main/java/me/chimkenu/expunge/listeners/game/SwingListener.java
@@ -1,78 +1,70 @@
 package me.chimkenu.expunge.listeners.game;
 
-import io.papermc.paper.event.player.PlayerArmSwingEvent;
+import me.chimkenu.expunge.Expunge;
+import me.chimkenu.expunge.enums.MeleeType;
 import me.chimkenu.expunge.game.GameManager;
-import me.chimkenu.expunge.items.weapons.melees.Chainsaw;
-import me.chimkenu.expunge.items.weapons.melees.Melee;
+import me.chimkenu.expunge.items.Gun;
+import me.chimkenu.expunge.items.Melee;
 import me.chimkenu.expunge.listeners.GameListener;
-import me.chimkenu.expunge.utils.Utils;
 import org.bukkit.*;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.damage.DamageSource;
+import org.bukkit.damage.DamageType;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.player.PlayerAnimationType;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
 public class SwingListener extends GameListener {
-    private final BreakGlassListener breakGlassListener;
-    private final HashSet<Player> playersUsingChainsaw;
+    private final Set<Player> playersUsingChainsaw;
+    private final Map<Player, Long> playersLastSwing;
 
-    public SwingListener(JavaPlugin plugin, GameManager gameManager, BreakGlassListener breakGlassListener) {
+    public SwingListener(Expunge plugin, GameManager gameManager) {
         super(plugin, gameManager);
-        this.breakGlassListener = breakGlassListener;
         playersUsingChainsaw = new HashSet<>();
+        playersLastSwing = new HashMap<>();
     }
 
-    private void swing(Player player, Melee melee) {
-        ArrayList<LivingEntity> entities = new ArrayList<>();
-        World world = player.getWorld();
-        Location loc = player.getEyeLocation();
-        for (int i = 0; i < melee.getRange(); i++) {
-            loc.add(loc.getDirection());
-
-            // check for glass to break
-            breakGlassListener.breakGlass(loc.getBlock());
-
-            for (Entity e : world.getNearbyEntities(loc, 0.5, 1, 0.5)) {
-                if (e == player) {
-                    continue;
-                }
-                if (!(e instanceof LivingEntity living)) {
-                    continue;
-                }
-                if (e instanceof ArmorStand) {
-                    continue;
-                }
-                if (e instanceof Pig) {
-                    continue;
-                }
-                if (e instanceof Player p && p.getGameMode() != GameMode.ADVENTURE) {
-                    continue;
-                }
-                if (entities.contains(living)) {
-                    continue;
-                }
-                entities.add(living);
-                if (entities.size() >= melee.getEntitiesToHit()) break;
-            }
-            if (entities.size() >= melee.getEntitiesToHit()) break;
-        }
-
-        // play sound depending on whether an entity was hit (only if playSound is true)
-        if (entities.isEmpty()) {
-            if (melee.getRange() <= 2) player.getWorld().playSound(loc, Sound.ENTITY_PLAYER_ATTACK_SWEEP, SoundCategory.PLAYERS, 1, 1);
-            else player.getWorld().playSound(loc, Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK, SoundCategory.PLAYERS, 1, 1);
+    private void swing(Player player, Entity target, Melee melee) {
+        playersLastSwing.putIfAbsent(player, 0L);
+        var debounce = System.currentTimeMillis() - playersLastSwing.get(player);
+        if (debounce < 100 || player.getAttackCooldown() != 1) {
             return;
         }
+        playersLastSwing.put(player, System.currentTimeMillis());
 
-        player.getWorld().playSound(loc, Sound.ENTITY_IRON_GOLEM_DAMAGE, SoundCategory.PLAYERS, 1, 1);
-        player.getWorld().playSound(loc, Sound.BLOCK_HONEY_BLOCK_STEP, SoundCategory.PLAYERS, 1, 1);
+        HashSet<LivingEntity> entities = new HashSet<>();
+        for (Entity e : player.getWorld().getNearbyEntities(target.getBoundingBox().expand(1, 0.25, 1))) {
+            if (e == player) {
+                continue;
+            }
+            if (!(e instanceof LivingEntity living)) {
+                continue;
+            }
+            if (e instanceof ArmorStand) {
+                continue;
+            }
+            if (e instanceof Player p && p.getGameMode() != GameMode.ADVENTURE) {
+                continue;
+            }
+            entities.add(living);
+            if (entities.size() >= melee.pierceNumber()) break;
+        }
+
+        player.getWorld().playSound(target, Sound.ENTITY_IRON_GOLEM_DAMAGE, SoundCategory.PLAYERS, 1, 1);
+        player.getWorld().playSound(target, Sound.BLOCK_HONEY_BLOCK_STEP, SoundCategory.PLAYERS, 1, 1);
+        player.getWorld().spawnParticle(Particle.SWEEP_ATTACK, player.getEyeLocation().add(player.getEyeLocation().getDirection().multiply(1.5)), 1);
         for (LivingEntity livingEntity : entities) {
-            livingEntity.getWorld().spawnParticle(Particle.BLOCK_CRACK, livingEntity.getLocation().add(0, .5, 0), 50, 0.2, 0.2, 0.2, Material.NETHER_WART_BLOCK.createBlockData());
+            livingEntity.getWorld().spawnParticle(Particle.BLOCK, livingEntity.getLocation().add(0, .5, 0), 50, 0.2, 0.2, 0.2, Material.NETHER_WART_BLOCK.createBlockData());
 
             /* most melee weapons do % damage rather than fixed
              * 5% tank
@@ -80,66 +72,97 @@ public class SwingListener extends GameListener {
              * 50% charger
              * 100% everything else
              */
-            if (melee.getDamage() == 0)
+            var damage = melee.damage();
+            var attribute = livingEntity.getAttribute(Attribute.MAX_HEALTH);
+            if (melee.damageType() == MeleeType.Damage.RELATIVE && attribute != null) {
+                var maxHealth = attribute.getValue();
                 switch (livingEntity.getType()) {
-                    case ZOGLIN -> livingEntity.damage((400 + (gameManager.getDifficulty().ordinal() * 200)) * 0.5, player);
-                    case IRON_GOLEM -> livingEntity.damage(2500 + (gameManager.getDifficulty().ordinal() * 1000) * 0.05, player);
-                    case ENDERMAN -> livingEntity.damage(1000 * 0.25, player);
-                    default -> livingEntity.damage((livingEntity.getHealth() + livingEntity.getAbsorptionAmount()) * 2, player);
+                    case ZOGLIN ->
+                            damage = maxHealth * 0.5;
+                    case IRON_GOLEM ->
+                            damage = maxHealth * 0.05;
+                    case ENDERMAN ->
+                            damage = maxHealth * 0.25;
+                    default ->
+                            damage = maxHealth;
                 }
-            else // fixed damage
-                livingEntity.damage(melee.getDamage(), player);
+
+                // avoid armor shenanigans. anyway, its better that it does more damage
+                damage += 5;
+            }
+
+            livingEntity.damage(damage, player);
         }
     }
 
-    private void handleSwing(Player player, ItemStack mainHand, Melee melee) {
-        if (player.getCooldown(mainHand.getType()) > 1) {
-            return;
-        }
-
-        player.setCooldown(mainHand.getType(), melee.getCooldown());
-        if (!(melee instanceof Chainsaw)) {
-            swing(player, melee);
+    private void handleSwing(Player player, Entity target, Melee melee) {
+        if (!Objects.equals(melee.id(), "CHAINSAW")) {
+            swing(player, target, melee);
             return;
         }
 
         if (playersUsingChainsaw.contains(player)) {
             return;
         }
-
         playersUsingChainsaw.add(player);
-        new BukkitRunnable() {
-            int i = 0;
-            @Override
-            public void run() {
-                if (player.getInventory().getItemInMainHand().getType() != melee.getMaterial()) {
-                    playersUsingChainsaw.remove(player);
-                    this.cancel();
-                }
-                if (i % 5 == 0) {
-                    Location loc = player.getEyeLocation().add(player.getLocation().getDirection().multiply(1.5));
-                    player.getWorld().spawnParticle(Particle.SWEEP_ATTACK, loc, 1);
-                    swing(player, melee);
-                }
-                i++;
-            }
-        }.runTaskTimer(plugin, 1, 1);
+
+        gameManager.addTask(
+                new BukkitRunnable() {
+                    int i = 0;
+                    @Override
+                    public void run() {
+                        if (player.getInventory().getItemInMainHand().getType() != melee.material()) {
+                            playersUsingChainsaw.remove(player);
+                            this.cancel();
+                        }
+                        if (i % 5 == 0) {
+                            Location loc = player.getEyeLocation().add(player.getLocation().getDirection().multiply(1.5));
+                            player.getWorld().spawnParticle(Particle.SWEEP_ATTACK, loc, 1);
+                            for (Entity e : player.getWorld().getNearbyEntities(loc, 1, 1, 1)) {
+                                if (e == player) {
+                                    continue;
+                                }
+                                if (!(e instanceof LivingEntity living)) {
+                                    continue;
+                                }
+                                if (e instanceof ArmorStand) {
+                                    continue;
+                                }
+                                if (e instanceof Player p && p.getGameMode() != GameMode.ADVENTURE) {
+                                    continue;
+                                }
+                                swing(player, living, melee);
+                                break;
+                            }
+                        }
+                        i++;
+                    }
+                }.runTaskTimer(plugin, 1, 1)
+        );
     }
 
     @EventHandler
-    public void onClick(PlayerArmSwingEvent e) {
-        Player player = e.getPlayer();
+    public void onDamage(EntityDamageByEntityEvent e) {
+        if (!(e.getDamager() instanceof Player player)) {
+            return;
+        }
+
         if (!gameManager.getPlayers().contains(player)) {
             return;
         }
 
-        ItemStack mainHand = player.getInventory().getItemInMainHand();
-        if (!(Utils.getGameItemFromItemStack(mainHand) instanceof Melee melee)) {
+        var heldItem = player.getInventory().getItemInMainHand();
+        var item = plugin.getItems().toGameItem(heldItem);
+        if (!(item instanceof Melee melee)) {
             return;
         }
 
-        e.setCancelled(true);
-        handleSwing(player, mainHand, melee);
+        // shove threshold (or spam attack)
+        if (e.getDamage() < 10) {
+            return;
+        }
+
+        handleSwing(player, e.getEntity(), melee);
     }
 
     @EventHandler
